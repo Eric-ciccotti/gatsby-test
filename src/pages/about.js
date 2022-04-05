8c7bb472266698b01b4685d65148fcf2084cf375
Eric <35898232+Eric-ciccotti@users.noreply.github.com>
2022-04-05 16:37:04 +0200

    ✨ ajout du plugin StaticImage et ces peer dependencies

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,12 @@
 import * as React from 'react';
-import { Link } from 'gatsby';
 import Layout from '../components/layout';
+import { StaticImage } from 'gatsby-plugin-image'
 
 const About = () => {
     return (
         <Layout pageTitle="Page about!">
             <p>C'est la page à propos de moi !</p>
+            <StaticImage src="./../images/moi.jpg" alt="ma photo" />
         </Layout>
 
     )
