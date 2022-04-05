f03edf8f0b397e731a935499a6086aaee6668dea
Eric <35898232+Eric-ciccotti@users.noreply.github.com>
2022-04-05 15:10:13 +0200

    ✨ about et layout pages avec nouveau module css

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { Link } from 'gatsby';
+import Layout from '../components/layout';
+
+const About = () => {
+    return (
+        <Layout pageTitle="Page about!">
+            <p>C'est la page à propos de moi !</p>
+        </Layout>
+
+    )
+}
+
+export default About;
